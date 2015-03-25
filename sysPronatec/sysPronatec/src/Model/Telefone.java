443837273f66edcd5bb305055b318c473ebd8dd1
David Silva <davidhenriquesi@live.com>
2015-03-25 12:53:50 -0300

    Atributos(Pacote Model Parcial)

---
+++ sysPronatec/sysPronatec/src/Model/Telefone.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author David
+ */
+public class Telefone {
+    
+    private int IdTelefone;
+    private Integer TelefoneCelular;
+    private Integer TelefoneResidencial;
+
+    
+}
